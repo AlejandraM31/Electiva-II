5fb234dc7eb5069f19145ea340135ef5fe622a93
AlejandraM31 <63984212+AlejandraM31@users.noreply.github.com>
2020-04-25 18:44:58 -0500

    cambios codigo
    Funcion operaciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 function onlyNums(e) {
-    const code = window.event ? e.which : e.ketCode;
+    var code = window.event?e.which:e.keyCode;
     return !(code < 48 || code > 57);
 }
 
 function begin() {
-    const vOne = document.getElementById('valueOne').value;
-    const vTwo = document.getElementById('valueTwo').value;
+    var vOne = document.getElementById('valueOne').value;
+    var vTwo = document.getElementById('valueTwo').value;
     //const options = document.getElementById('opt').options
 
-    const dates = [
+    var dates = [
 
         {
             code: "32423",
@@ -24,7 +24,7 @@ function begin() {
         }
 ]
 
-    dates.forEach((record)=>{
+    dates.forEach((record) =>{
         alert(`Codigo=${record.code} Name=${record.name}`)
     })
 
@@ -39,4 +39,30 @@ $(document).ready(function () {
        $('#layout').fadeIn('slow');
     });
 })
+function operaciones(){
+        var nOne = document.getElementById("numOne").value;
+        nOne = parseInt(nOne);
+        var nTwo = document.getElementById("numTwo").value;
+        nTwo = parseInt(nTwo);
+
+        var option = document.getElementById('option');
+       alert(option.value);
+        switch (option.selectedIndex) {
+          case 1: result = nOne + nTwo;
+            break;
+          case 2:
+            result = nOne>nTwo?nOne-nTwo: nTwo-nOne;
+            break;
+          case 3:
+          result = nOne*nTwo;
+          break;
+          case 4:
+            result =  nOne>nTwo? nOne/nTwo:0;
+            break;
+          default:
+        }
+        var r = document.getElementById("result");
+        r.innerHTML = "El resultado es " + result;
+      }
+
 
