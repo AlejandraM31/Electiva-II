0e8ca6daad8e4e74e288e17db5d4f220344cb445
AlejandraM31 <63984212+AlejandraM31@users.noreply.github.com>
2020-05-30 19:08:16 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -44,82 +44,78 @@ function operaciones(){
         nOne = parseInt(nOne);
         var nTwo = document.getElementById("numTwo").value;
         nTwo = parseInt(nTwo);
-
         var option = document.getElementById('option');
-       alert(option.value);
+        alert(option.value);
         switch (option.selectedIndex) {
           case 1: result = nOne + nTwo;
-            break;
+          break;
           case 2:
-            if(nOne>nTwo){
-                result=nOne-nTwo;
-            }else{
-             result=nTwo-nOne;
-             }
-            break;
+          if(nOne>nTwo){
+            result=nOne-nTwo;
+          }else{
+            result=nTwo-nOne;
+          }
+          break;
           case 3:
           result = nOne*nTwo;
           break;
           case 4:
             result =  nOne>nTwo? nOne/nTwo:nTwo/nOne;
-                if(nTwo==0){
-                    alert("La division por cero no existe,los numeros ingresados no pueden ser cero");
-                }
-            break;
-            case 5:
-                result=Math.pow(nOne,nTwo);
-                break;
-            case 6:
-               var  dividendo;
-               var  divisor;
-               var  resto;
-                if(nOne>nTwo){
-                    dividendo=nOne;
-                    divisor=nTwo;
-                }else{
-                    dividendo=nTwo;
-                    divisor=nOne;
-                }
-                resto=dividendo%divisor;
-                do{
-                    dividendo=divisor;
-                    divisor=resto;
-                    resto=dividendo%divisor;
-                }
-                while(resto!=0);
+          if(nTwo==0){
+            alert("La division por cero no existe,los numeros ingresados no pueden ser cero");
+          }
+          break;
+          case 5:
+          result=Math.pow(nOne,nTwo);
+          break;
+          case 6:
+          var  dividendo;
+          var  divisor;
+          var  resto;
+          if(nOne>nTwo){
+            dividendo=nOne;
+            divisor=nTwo;
+          }else{
+            dividendo=nTwo;
+            divisor=nOne;
+          }
+          resto=dividendo%divisor;
+          do{
+           dividendo=divisor;
+           divisor=resto;
+           resto=dividendo%divisor;
+           }while(resto!=0);
                 result="El MCD de: " + nOne + " y " + nTwo + " es:  " +divisor;
                 alert("El MCD es : "+ divisor);
-             break;
+         break;
             case 7:
               // alert("Numeros amigos");
-                var cont;
-                var sumaDiv=0;
-                for(cont=1;cont<nOne;cont++){ //for para sumar los divisores propios del numero uno
-                    if(nOne%cont==0){
-                        sumaDiv=sumaDiv+cont;
-                    }
-                }
+             var cont;
+             var sumaDiv=0;
+             for(cont=1;cont<nOne;cont++){ //for para sumar los divisores propios del numero uno
+                 if(nOne%cont==0){
+                     sumaDiv=sumaDiv+cont;
+                 }
+             }
                 //si la suma de los divisores de nOne es igual a nTwo
-                if(sumaDiv==nTwo){
-                    sumaDiv=0;
-                    for(cont=1;cont<nTwo;cont++){ //sumo los divisores propios de nTwo
-                        if(nTwo%cont==0){
-                            sumaDiv=sumaDiv+cont;
-                        }
-                    }
+             if(sumaDiv==nTwo){
+                 sumaDiv=0;
+             for(cont=1;cont<nTwo;cont++){ //sumo los divisores propios de nTwo
+                  if(nTwo%cont==0){
+                   sumaDiv=sumaDiv+cont;
+                  }
+                  }
                 //si la suma de los divisores de nTwo es igual  a nOne 
-                 if(sumaDiv==nOne){
+              if(sumaDiv==nOne){
                    result="Los numeros SÍ son Amigos";
-                }else{
+              }else{
                    result="Los numeros NO son Amigos";
-                }
-                }
-                else{
-                    result="Los numeros NO son Amigos";
-                }
-        
-                   
-                break;
+              }
+              }
+              else{
+                result="Los numeros NO son Amigos";
+              }
+           break;
                case 8:
                 alert("numeros primos table")
                 var less=nOne<nTwo?nOne:nTwo;
